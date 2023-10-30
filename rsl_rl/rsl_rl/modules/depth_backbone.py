4e4bb1ff33fec11aea79689b562e7953fa838490
fishjohn <1258676867@qq.com>
2023-10-30 20:07:28 +0800

    add depth backbone network

---
+++ rsl_rl/rsl_rl/modules/depth_backbone.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+
+class RecurrentDepthBackbone(nn.Module):
+    def __init__(self, base_backbone, n_proprio, vision_laten_dim) -> None:
+        super().__init__()
+        activation = nn.ELU()
+        last_activation = nn.Tanh()
+        self.base_backbone = base_backbone
+        self.combination_mlp = nn.Sequential(
+            nn.Linear(vision_laten_dim + n_proprio, 128),
+            activation,
+            nn.Linear(128, 32)
+        )
+        self.rnn = nn.GRU(input_size=32, hidden_size=512, batch_first=True)
+        self.output_mlp = nn.Sequential(
+            nn.Linear(512, 32 + 2),
+            last_activation
+        )
+        self.hidden_states = None
+
+    def forward(self, depth_image, proprioception):
+        depth_image = self.base_backbone(depth_image)
+        depth_latent = self.combination_mlp(torch.cat((depth_image, proprioception), dim=-1))
+        depth_latent, self.hidden_states = self.rnn(depth_latent[:, None, :], self.hidden_states)
+        depth_latent = self.output_mlp(depth_latent.squeeze(1))
+
+        return depth_latent
+
+    def detach_hidden_states(self):
+        self.hidden_states = self.hidden_states.detach().clone()
+
+
+class DepthOnlyFCBackbone58x87(nn.Module):
+    def __init__(self, prop_dim, scandots_output_dim, hidden_state_dim, output_activation=None, num_frames=1):
+        super().__init__()
+
+        self.num_frames = num_frames
+        activation = nn.ELU()
+        self.image_compression = nn.Sequential(
+            # [1, 58, 87]
+            nn.Conv2d(in_channels=self.num_frames, out_channels=32, kernel_size=5),
+            # [32, 54, 83]
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # [32, 27, 41]
+            activation,
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            activation,
+            nn.Flatten(),
+            # [32, 25, 39]
+            nn.Linear(64 * 25 * 39, 128),
+            activation,
+            nn.Linear(128, scandots_output_dim)
+        )
+
+        if output_activation == "tanh":
+            self.output_activation = nn.Tanh()
+        else:
+            self.output_activation = activation
+
+    def forward(self, images: torch.Tensor):
+        images_compressed = self.image_compression(images.unsqueeze(1))
+        latent = self.output_activation(images_compressed)
+
+        return latent
